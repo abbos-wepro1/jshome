1a5704105f560310e66a79ec09347487ad964414
abbos-wepro1 <116338606+abbos-wepro1@users.noreply.github.com>
2022-10-27 23:06:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let clientName = prompt('What is ur name?')
+if (clientName === 'Alex') {
+  console.log('Welcome')
+} else {
+  console.log('Пользователь не найден,досвидули')
+}
+
+let number = prompt('What is ur cart number?')
+if (number === '7777'){
+
+  let balance = prompt ('cash out ')
+if (balance === '10000') 
+console.log('Все отлично')
+} else{
+  console.log('EROR 404')
+}
